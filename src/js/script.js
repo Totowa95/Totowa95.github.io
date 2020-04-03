4cf4c017ec21654ed2c187a7971efab2e10ab441
Anton <tonu4@ukr.net>
2020-04-03 17:24:53 +0300

    added animation

--- src/js/script.js
+++ src/js/script.js
@@ -109,4 +109,20 @@ $(document).ready(function(){
         });
         return false;
     });
+
+    //пейджап позже появляется
+    $(window).scroll(function() {
+        if ($(this).scrollTop()>1600) {
+            $('.pageup').fadeIn();
+        } else{
+            $('.pageup').fadeOut();
+        }
+    });
+        //плавный скрол
+        $("a[href^='#']").click(function(){
+            const _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+    });
+    new WOW().init();
   });
